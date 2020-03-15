ee8991388612a88368275f4e09d215e163b4c91c
cale <caleanderson9@gmail.com>
2020-03-15 11:13:41 -0500

    index route working correctly

--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -1,13 +1,21 @@
 class Api::SuppliersController < ApplicationController
   def index
+    @suppliers = Supplier.all
     render "index.json.jb"
   end
 
   def show
+    @supplier = Supplier.find_by(id: params[:id])  
     render "show.json.jb"
   end
 
   def create
+    Supplier.new(
+      name: params[:name],
+      email: params[:email],
+      phone_number: params[:phone_number]
+    )
+    
     render "show.json.jb"
   end
 
--- config/routes.rb
+++ config/routes.rb
@@ -16,6 +16,7 @@ Rails.application.routes.draw do
     get "/suppliers" => "suppliers#index"
     get "suppliers/:id" => "suppliers#show"
     post "suppliers" => "suppliers#create"
-
+    patch "suppliers/:id" => "suppliers#update"
+    delete "suppliers/:id" => "suppliers#destroy"
   end
 end
