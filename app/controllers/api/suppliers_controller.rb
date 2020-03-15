e894b44015f7b8b105014a83baa05aeb2f5a2cef
cale <caleanderson9@gmail.com>
2020-03-15 12:10:05 -0500

    add model method for supplier

--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -10,20 +10,26 @@ class Api::SuppliersController < ApplicationController
   end
 
   def create
-    Supplier.new(
+    @supplier = Supplier.new(
       name: params[:name],
       email: params[:email],
       phone_number: params[:phone_number]
     )
-    
+    @supplier.save!
     render "show.json.jb"
   end
 
   def update
+    @supplier = Supplier.find_by(id: params[:id]) 
+    @supplier.name = params[:name] || @supplier.name
+    @supplier.email = params[:email] || @supplier.email
+    @supplier.phone_number = params[:phone_number] || @supplier.phone_number 
+    @supplier.save!
     render "show.json.jb"
   end
 
   def destroy
+    @supplier = Supplier.find_by(id: params[:id]) 
     @supplier.destroy
     render json: {message: "Supplier successfuly deleted."}
   end
