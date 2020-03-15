93a411e9d9010fb7e962264b9b3c530073e850fb
cale <caleanderson9@gmail.com>
2020-03-15 10:47:46 -0500

    add all routes for supplier

--- app/controllers/api/suppliers_controller.rb
+++ app/controllers/api/suppliers_controller.rb
@@ -2,4 +2,21 @@ class Api::SuppliersController < ApplicationController
   def index
     render "index.json.jb"
   end
+
+  def show
+    render "show.json.jb"
+  end
+
+  def create
+    render "show.json.jb"
+  end
+
+  def update
+    render "show.json.jb"
+  end
+
+  def destroy
+    @supplier.destroy
+    render json: {message: "Supplier successfuly deleted."}
+  end
 end
